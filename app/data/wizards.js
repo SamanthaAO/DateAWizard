792c9443904fc72e0718ed6f825bc53ccd2507ae
SamanthaAO <samanthaaorcutt@gmail.com>
2019-06-23 21:54:45 -0700

    created rudimentary server that will display html pages

---
+++ app/data/wizards.js
@@ -0,0 +1,54 @@
+
+var wizards = [
+    {
+        name:"Harry Potter",
+        photo:"..images/harryPotter.gif",
+        scores:[
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+            ]
+
+    },
+    {
+        name:"Ron Weasley",
+        photo:"..images/ronWeasley.gif",
+        scores:[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+            ]
+    },
+    {
+        name:"Hermione Granger",
+        photo:"..images/hermioneGranger.gif",
+        scores:[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+            ]
+    }
+];
+
+module.exports = wizards;
