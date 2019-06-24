70faeb6c1e98973bf27672a9bb4bc526b2f5d56e
SamanthaAO <samanthaaorcutt@gmail.com>
2019-06-24 13:44:19 -0700

    created footer with links to wizard and muggle api lists and git hub repo. created routes so that links display apis

---
+++ app/data/muggles.js
@@ -0,0 +1,21 @@
+var muggles = [
+    {
+        name:"sam",
+        photo:"..images/harryPotter.gif",
+        scores:[
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+            ]
+
+    },
+];
+
+module.exports = muggles;
