3c5a938657cd871b550a7ab1aa1a6fe1bf0d05db
SamanthaAO <samanthaaorcutt@gmail.com>
2019-06-25 16:22:27 -0700

    made answers required and updated wizard data and made results display look nicer.

--- app/data/wizards.js
+++ app/data/wizards.js
@@ -14,12 +14,13 @@ var wizards = [
             5,
             5,
             5
-            ]
+            ],
+        description: "I am the chosen one. Well done for you if you match with me. My accomplishments include defeating the Dark Lord, and well I mean what more do you need to be impressed?"
 
     },
     {
         name:"Ron Weasley",
-        photo:"https://media.giphy.com/media/o5HKScC1PflLO/giphy.gif",
+        photo:"https://media.giphy.com/media/KGj95n858MTGo/giphy.gif",
         scores:[
             1,
             1,
@@ -31,11 +32,12 @@ var wizards = [
             1,
             1,
             1
-            ]
+            ],
+        description: "Hello, it's Ron here. I come from a large family, and if you are lookign for a sugar daddy type situationt hen you are barking up the wrong tree.  My hobies include eating everything, quidditch, and getting in over my head on adventures with my friends. Dislikes: Spiders."
     },
     {
         name:"Hermione Granger",
-        photo:"https://media.giphy.com/media/o5HKScC1PflLO/giphy.gif",
+        photo:"https://media.giphy.com/media/l01fjs9HRzU08/giphy.gif",
         scores:[
             2,
             2,
@@ -47,7 +49,195 @@ var wizards = [
             2,
             2,
             2
-            ]
+            ],
+        description: "Born to unsuspecting muggle dentist parents I have since blossomed into brightest witch of my generation, so if you are ready to be wowed by my intelect you have come to the right place. I also enjoy cats... but not being one. "
+    },
+    {
+        name:"Voldemort",
+        photo:"https://media0.giphy.com/media/54Q8WBE4zDN5e/giphy.gif",
+        scores:[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+            ],
+        description: "Pleasure to meet you. Friends call me the Dark Lord. My passions include: world domintation, snakes, looking like a snake, talking to nakes, and listening to the thoughts of a teenage boy through a special connection that we have. Muggles need not reply. "
+    },
+    {
+        name:"Albus Dumbledore",
+        photo:"https://media.giphy.com/media/nQfQayikD5rX2/giphy.gif",
+        scores:[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+            ],
+        description: "If you are looking for an old soul then you have come to the right place. I am as old as they get, but with that age somes a lot of wisdom... not all of which I fully remember but that is what the good old pensive is for am I right? I am considered by many to be one of the most powerful wizards of all time...but I am not one to brag."
+    },
+    {
+        name:"Severous Snape",
+        photo:"https://media1.tenor.com/images/3d09778caf87164047352ff739d7b844/tenor.gif?itemid=4059508",
+        scores:[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+            ],
+        description: "Is your name Lily Pottter? Well if not I am sorry to tell you that I am not interested...."
+    },
+    {
+        name:"Draco Malfoy",
+        photo:"https://media3.giphy.com/media/ygx8EYsdRkNpu/giphy.gif",
+        scores:[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+            ],
+        description: "Upon first introduction you might think that I am a bit of a jerk, but  when you take a look deeper you will see that I am really just being crushed by the unrealistic expectation of my parents and the dark lord. However, when you then look even deeper you will see I am kinda still a jerk, so if you are in for a bad boy with mommy issues then come on down. "
+    },
+    {
+        name:"Sirius Black",
+        photo:"https://data.whicdn.com/images/247385068/original.gif",
+        scores:[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+            ],
+        description: "If you like dogs then you will like me. Spent the last few years in Azkaban, totally was not my fault. Very commited to my godson. Kinda a shut in..., but if you are down to Netflix and Chill then hit me up. "
+    },
+    {
+        name:"Neville Longbottom",
+        photo:"http://24.media.tumblr.com/tumblr_m28jooqtMp1qe9m8po3_r3_250.gif",
+        scores:[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+            ],
+        description: "I am definately a late bloomer, but believe me I am worth the wait. I also love plants, my grandma, and hanging out with my DA pals."
+    },
+    {
+        name:"Rubeus Hagrid",
+        photo:"https://media.giphy.com/media/7hnNiwfkYsPFC/200.gif",
+        scores:[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+            ],
+        description: "LOVE magical creatures, especially dragons. Live in a sweet shack on the edge of the hogwarts campus (location, location, location, am I right). Prefer larger woman."
+    },
+    {
+        name:"Remus Lupin",
+        photo:"https://i.gifer.com/Gdhe.gif",
+        scores:[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+            ],
+        description: "I have managed some mischeif in my days. Not available for dates on full moons... "
+    },
+    {
+        name:"Luna Lovegood",
+        photo:"https://media2.giphy.com/media/BxYmEq7Ksqlbi/giphy.gif",
+        scores:[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+            ],
+        description: "Some say I am crazy... but I prefer excentric. I am always ready for a good time as long as my shoes have not mysteriously disappeared due to the Nargles."
+    },
+    {
+        name:"Fred and George Weasley",
+        photo:"https://media0.giphy.com/media/DdR7RSVKkGs4E/giphy.gif",
+        scores:[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+            ],
+        description: "Prosperous shop owners in the center of Diagon Ally looking to find the perfect souldmate to share all their best jokes with. Buy one get one half off..."
+    },
+    {
+        name:"Peter Pettigrew",
+        photo:"https://vignette.wikia.nocookie.net/harrypotter/images/d/d4/Pettigrew_to_Wormtail.gif/revision/latest/scale-to-width-down/220?cb=20090305023121",
+        scores:[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+            ],
+        description: "I swear I will be your very best friend in the entire world. I will be so incredibly loyal, and wil 100% not give you up to the dark lord."
     }
 ];
 
